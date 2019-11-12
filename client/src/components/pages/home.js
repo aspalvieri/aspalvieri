a2d36c72b4a8ccce1c45be47e2e4e7031ede68f2
aspalvieri <alex.spalvieri@hotmail.com>
2019-11-12 03:27:32 -0500

    push

---
+++ client/src/components/pages/home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return(
+        <div className="container">
+            <header>
+                <h1 style={{textAlign: "center"}}>Home Page</h1>
+            </header>
+            <p>
+                This is the home page!
+            </p>
+        </div>
+    );
+}
+
+export default Home;
