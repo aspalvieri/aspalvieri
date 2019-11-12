bd306fbc4ad9d9630c05378f365bd6838728f1ea
aspalvieri <alex.spalvieri@hotmail.com>
2019-11-12 04:56:53 -0500

    array fetching

--- client/src/components/pages/home.js
+++ client/src/components/pages/home.js
@@ -4,10 +4,15 @@ import Axios from "axios";
 function Home() {
     const [array, setArray] = useState([]);
 
-    useEffect(() => {
+    function fetchRandomArray() {
         Axios.get("/api/test/random_array")
             .then(res => setArray(res.data))
             .catch(err => console.log(err));
+    }
+
+    //Passing empty array as 2nd argument causes this to run only once on page load
+    useEffect(() => {
+        fetchRandomArray();
     }, []);
 
     return(
@@ -16,22 +21,23 @@ function Home() {
                 <h1 style={{textAlign: "center"}}>Home Page</h1>
             </header>
             <p>
+                <button onClick={() => fetchRandomArray()}>Fetch</button> <br/>
                 Random Array:
-                <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Array (size of {array.length})</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {array.map(arr => (
-                            <tr>
-                                <td>{arr}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
             </p>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Array (size of {array.length})</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {array.map((arr, i) => (
+                        <tr key={i}>
+                            <td><span className="gray">{i}:</span> {arr}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     );
 }
