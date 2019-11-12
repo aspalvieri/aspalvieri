2dbe59df3293447ad131b381e459dec8cec69b01
aspalvieri <alex.spalvieri@hotmail.com>
2019-11-12 03:04:01 -0500

    message

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// Body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Routes
+const routes = require("./routes.js");
+app.use("/api", routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = (process.env.PORT || 4000);
+app.listen(port, () => console.log(`Listening on ${port}`));
---
+++ routes.js
@@ -0,0 +1,12 @@
+// Our Express app module
+const express = require('express');
+const app = express();
+
+// Importing the routes
+//const pagesRoutes = require('./routes/pages.js');
+
+// Registering our pageRoutes
+//app.use('/', pagesRoutes);
+
+// Exporting the changes
+module.exports = app;
