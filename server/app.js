de0122fb70650d095f0aa27d677e2693ce455add
aspalvieri <alex.spalvieri@hotmail.com>
2022-01-22 04:24:15 -0500

    package update

--- server/app.js
+++ server/app.js
@@ -48,7 +48,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
-const port = (process.env.PORT || 8080);
+const port = (process.env.PORT || 5000);
 app.listen(port, () => console.log(`Server is running on port ${port}.`));
 
 module.exports = { app };
